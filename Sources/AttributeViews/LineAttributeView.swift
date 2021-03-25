a97c995278172787c958753e06c40b1a6c95f012
Callum McColl <callum.mccoll1@gmail.com>
2021-03-25 15:02:42 +1000

    Revert LineAttributeView

--- Sources/AttributeViews/LineAttributeView.swift
+++ Sources/AttributeViews/LineAttributeView.swift
@@ -15,47 +15,48 @@ import Attributes
 
 public struct LineAttributeView<Config: AttributeViewConfig>: View {
     
-    @Binding var attribute: LineAttribute
-    @Binding var errors: [String]
-    let label: String
+    let subView: () -> AnyView
     
     public init<Root: Modifiable>(root: Binding<Root>, path: Attributes.Path<Root, LineAttribute>, label: String) {
-        self.init(
-            attribute: Binding(
-                get: { root.wrappedValue[keyPath: path.keyPath] },
-                set: {
-                    _ = try? root.wrappedValue.modify(attribute: path, value: $0)
-                }
-            ),
-            errors: Binding(
-                get: { root.wrappedValue.errorBag.errors(forPath: AnyPath(path)).map(\.message) },
-                set: { _ in }
-            ),
-            label: label
-        )
+        self.subView = {
+            switch root.wrappedValue[keyPath: path.keyPath].type {
+            case .bool:
+                return AnyView(BoolView<Config>(root: root, path: path.boolValue, label: label))
+            case .integer:
+                return AnyView(IntegerView<Config>(root: root, path: path.integerValue, label: label))
+            case .float:
+                return AnyView(FloatView<Config>(root: root, path: path.floatValue, label: label))
+            case .expression(let language):
+                return AnyView(ExpressionView<Config>(root: root, path: path.expressionValue, label: label, language: language))
+            case .enumerated(let validValues):
+                return AnyView(EnumeratedView<Config>(root: root, path: path.enumeratedValue, label: label, validValues: validValues))
+            case .line:
+                return AnyView(LineView<Config>(root: root, path: path.lineValue, label: label))
+            }
+        }
     }
     
     init(attribute: Binding<LineAttribute>, errors: Binding<[String]> = .constant([]), label: String) {
-        self._attribute = attribute
-        self._errors = errors
-        self.label = label
+        self.subView = {
+            switch attribute.wrappedValue.type {
+            case .bool:
+                return AnyView(BoolView<Config>(value: attribute.boolValue, errors: errors, label: label))
+            case .integer:
+                return AnyView(IntegerView<Config>(value: attribute.integerValue, errors: errors, label: label))
+            case .float:
+                return AnyView(FloatView<Config>(value: attribute.floatValue, errors: errors, label: label))
+            case .expression(let language):
+                return AnyView(ExpressionView<Config>(value: attribute.expressionValue, errors: errors, label: label, language: language))
+            case .enumerated(let validValues):
+                return AnyView(EnumeratedView<Config>(value: attribute.enumeratedValue, errors: errors, label: label, validValues: validValues))
+            case .line:
+                return AnyView(LineView<Config>(value: attribute.lineValue, errors: errors, label: label))
+            }
+        }
     }
     
     public var body: some View {
-        switch attribute.type {
-        case .bool:
-            BoolView<Config>(value: $attribute.boolValue, errors: $errors, label: label)
-        case .integer:
-            IntegerView<Config>(value: $attribute.integerValue, errors: $errors, label: label)
-        case .float:
-            FloatView<Config>(value: $attribute.floatValue, errors: $errors, label: label)
-        case .expression(let language):
-            ExpressionView<Config>(value: $attribute.expressionValue, errors: $errors, label: label, language: language)
-        case .enumerated(let validValues):
-            EnumeratedView<Config>(value: $attribute.enumeratedValue, errors: $errors, label: label, validValues: validValues)
-        case .line:
-            LineView<Config>(value: $attribute.lineValue, errors: $errors, label: label)
-        }
+        subView()
     }
 }
 
