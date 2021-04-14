b678263ad5b4e06470c68c8bbc5bfb151229fda7
Callum McColl <callum.mccoll1@gmail.com>
2021-04-14 14:46:24 +1000

    Created EnumerableCollection preview

--- Sources/AttributeViews/EnumerableCollectionView.swift
+++ Sources/AttributeViews/EnumerableCollectionView.swift
@@ -16,29 +16,33 @@ import Attributes
 public struct EnumerableCollectionView<Config: AttributeViewConfig>: View {
     
     @Binding var value: Set<String>
-    @State var errors: [String]
+    @Binding var errors: [String]
+    
     let label: String
     let validValues: Set<String>
     
     @EnvironmentObject var config: Config
     
     public init<Root: Modifiable>(root: Binding<Root>, path: Attributes.Path<Root, Set<String>>, label: String, validValues: Set<String>) {
-        let errors = State<[String]>(initialValue: root.wrappedValue.errorBag.errors(forPath: AnyPath(path)).map { $0.message })
-        self._errors = errors
-        self._value = Binding(
-            get: { root.wrappedValue[keyPath: path.keyPath] },
-            set: {
-                _ = try? root.wrappedValue.modify(attribute: path, value: $0)
-                errors.wrappedValue = root.wrappedValue.errorBag.errors(forPath: AnyPath(path)).map { $0.message }
-            }
+        self.init(
+            value: Binding(
+                get: { root.wrappedValue[keyPath: path.keyPath] },
+                set: {
+                    _ = try? root.wrappedValue.modify(attribute: path, value: $0)
+                }
+            ),
+            errors: Binding(
+                get: { root.wrappedValue.errorBag.errors(forPath: AnyPath(path)).map(\.message) },
+                set: { _ in }
+            ),
+            label: label,
+            validValues: validValues
         )
-        self.label = label
-        self.validValues = validValues
     }
     
-    init(value: Binding<Set<String>>, label: String, validValues: Set<String>) {
+    init(value: Binding<Set<String>>, errors: Binding<[String]> = .constant([]), label: String, validValues: Set<String>) {
         self._value = value
-        self._errors = State<[String]>(initialValue: [])
+        self._errors = errors
         self.label = label
         self.validValues = validValues
     }
@@ -68,6 +72,57 @@ public struct EnumerableCollectionView<Config: AttributeViewConfig>: View {
                     }
                 }
             }
+        }
+    }
+    
+}
+
+struct EnumerableCollectionView_Previews: PreviewProvider {
+    
+    struct Root_Preview: View {
+        
+        @State var modifiable: EmptyModifiable = EmptyModifiable(attributes: [
+            AttributeGroup(
+                name: "Fields", fields: [Field(name: "enumerableCollection", type: .enumerableCollection(validValues: ["a", "b", "c", "d", "e", "f"]))], attributes: ["enumerableCollection": .enumerableCollection(["a", "b", "e", "f"], validValues: ["a", "b", "c", "d", "e", "f"])], metaData: [:])
+        ])
+        
+        let path = EmptyModifiable.path.attributes[0].attributes["enumerableCollection"].wrappedValue.enumerableCollectionValue
+        
+        let config = DefaultAttributeViewsConfig()
+        
+        var body: some View {
+            EnumerableCollectionView<DefaultAttributeViewsConfig>(
+                root: $modifiable,
+                path: path,
+                label: "Root",
+                validValues: ["a", "b", "c", "d", "e", "f"]
+            ).environmentObject(config)
+        }
+        
+    }
+    
+    struct Binding_Preview: View {
+        
+        @State var value: Set<String> = ["A", "D", "F"]
+        @State var errors: [String] = ["An error", "A second error"]
+        
+        let config = DefaultAttributeViewsConfig()
+        
+        var body: some View {
+            EnumerableCollectionView<DefaultAttributeViewsConfig>(
+                value: $value,
+                errors: $errors,
+                label: "Binding",
+                validValues: ["A", "B", "C", "D", "E", "F"]
+            ).environmentObject(config)
+        }
+        
+    }
+    
+    static var previews: some View {
+        VStack {
+            Root_Preview()
+            Binding_Preview()
         }
     }
 }
