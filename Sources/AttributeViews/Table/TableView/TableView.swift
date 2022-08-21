5f26cdf9edcfd616ca30f6fe71798fd0150ca32a
Callum McColl <callum.mccoll1@gmail.com>
2022-08-21 12:08:05 +1000

    Made TableView and associated view models pass the linter

--- Sources/AttributeViews/Table/TableView/TableView.swift
+++ Sources/AttributeViews/Table/TableView/TableView.swift
@@ -6,8 +6,8 @@
 //
 
 #if canImport(TokamakShim)
-import TokamakShim
 import Foundation
+import TokamakShim
 #else
 import SwiftUI
 #endif
@@ -15,14 +15,27 @@ import SwiftUI
 import Attributes
 import GUUI
 
+// swiftlint:disable type_contents_order
+
+/// A view that displays a table of attributes.
+/// 
+/// The table contains a header and a body. The header contains the
+/// title of the table with the errors vertically stacked below the title.
+/// The body contains vertically stacked rows with contents of the table.
+/// At the bottom of the table are input fields for adding a new row.
 public struct TableView: View {
 
+    /// The view model associated with this view.
     @ObservedObject var viewModel: TableViewModel
 
+    /// Create a new `TableView`.
+    /// 
+    /// - Parameter viewModel: The view model associated with this view.
     public init(viewModel: TableViewModel) {
         self._viewModel = ObservedObject(wrappedValue: viewModel)
     }
 
+    /// The contents of this view.
     public var body: some View {
         VStack(alignment: .leading) {
             Text(viewModel.label.pretty.capitalized)
@@ -40,11 +53,16 @@ public struct TableView: View {
 }
 
 #if canImport(SwiftUI)
+
+/// The previews of associated with `TableView`.
 struct TableView_Previews: PreviewProvider {
 
+    /// A view that renders a `TableView` preview containing
+    /// a table that exist within a `Modifiable` object.
     struct Root_Preview: View {
 
-        @State var modifiable: EmptyModifiable = EmptyModifiable(attributes: [
+        /// The `Modifiable` object containing the table.
+        @State var modifiable = EmptyModifiable(attributes: [
             AttributeGroup(
                 name: "Fields",
                 fields: [
@@ -54,35 +72,47 @@ struct TableView_Previews: PreviewProvider {
                     )
                 ],
                 attributes: [
-                    "table": .table([
-                        [.bool(false), .integer(1), .float(1.1)],
-                        [.bool(true), .integer(2), .float(2.2)]
-                    ], columns: [("b", .bool), ("i", .integer), ("f", .float)]
+                    "table": .table(
+                        [
+                            [.bool(false), .integer(1), .float(1.1)],
+                            [.bool(true), .integer(2), .float(2.2)]
+                        ],
+                        columns: [("b", .bool), ("i", .integer), ("f", .float)]
                     )
                 ],
                 metaData: [:]
             )
         ])
 
+        /// The path to the collection within `modifiable`.
         let path = EmptyModifiable.path.attributes[0].attributes["table"].wrappedValue.tableValue
 
+        /// The content of this view.
         var body: some View {
             TableViewPreviewView(
                 viewModel: TableViewModel(
                     root: Ref(get: { self.modifiable }, set: { self.modifiable = $0 }),
                     path: path,
                     label: "Root",
-                    columns: [.init(name: "b", type: .bool), .init(name: "i", type: .integer), .init(name: "f", type: .float)]
+                    columns: [
+                        .init(name: "b", type: .bool),
+                        .init(name: "i", type: .integer),
+                        .init(name: "f", type: .float)
+                    ]
                 )
             )
         }
 
     }
 
+    /// A view that renders a `TableView` preview containing
+    /// a table that does not exist within a `Modifiable` object.
     struct Binding_Preview: View {
 
+        /// The table.
         @State var value: [[LineAttribute]] = []
 
+        /// The contents of this view.
         var body: some View {
             TableViewPreviewView(
                 viewModel: TableViewModel(
@@ -104,21 +134,28 @@ struct TableView_Previews: PreviewProvider {
 
     }
 
+    /// A view that provides a @StateObject `TableViewModel` that is passed to
+    /// a `TableView`.
     struct TableViewPreviewView: View {
 
+        /// The view model associated with the `TableView`.
         @StateObject var viewModel: TableViewModel
 
+        /// Create a new `TableView`, passing `viewModel` to it.
         var body: some View {
             TableView(viewModel: viewModel)
         }
 
     }
 
+    /// All previews associated with `TableView`.
     static var previews: some View {
         VStack {
             Root_Preview()
             Binding_Preview()
         }
     }
+
 }
+
 #endif
