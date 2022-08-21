70cff920bffdd2ed91c0b38c5642c09e49b92f21
Callum McColl <callum.mccoll1@gmail.com>
2022-08-21 10:35:24 +1000

    Made LineAttributeView and associated view models pass the linter

--- Sources/AttributeViews/LineAttribute/LineAttributeViewModel.swift
+++ Sources/AttributeViews/LineAttribute/LineAttributeViewModel.swift
@@ -65,106 +65,81 @@ import SwiftUI
 import Attributes
 import GUUI
 
-fileprivate final class LineAttributeValue: Value<LineAttribute> {
-
-    private let _boolViewModel: () -> BoolViewModel
-
-    private let _integerViewModel: () -> IntegerViewModel
-
-    private let _floatViewModel: () -> FloatViewModel
-
-    private let _expressionViewModel: () -> ExpressionViewModel
-
-    private let _enumeratedViewModel: () -> EnumeratedViewModel
-
-    private let _lineViewModel: () -> LineViewModel
-
-    var boolViewModel: BoolViewModel { _boolViewModel() }
-
-    var integerViewModel: IntegerViewModel { _integerViewModel() }
-
-    var floatViewModel: FloatViewModel { _floatViewModel() }
-
-    var expressionViewModel: ExpressionViewModel { _expressionViewModel() }
-
-    var enumeratedViewModel: EnumeratedViewModel { _enumeratedViewModel() }
-
-    var lineViewModel: LineViewModel { _lineViewModel() }
-
-    init<Root: Modifiable>(root: Ref<Root>, path: Attributes.Path<Root, LineAttribute>, label: String, notifier: GlobalChangeNotifier? = nil) {
-        self._boolViewModel = {
-            BoolViewModel(root: root, path: path.boolValue, label: label, notifier: notifier)
-        }
-        self._integerViewModel = {
-            IntegerViewModel(root: root, path: path.integerValue, label: label, notifier: notifier)
-        }
-        self._floatViewModel = {
-            FloatViewModel(root: root, path: path.floatValue, label: label, notifier: notifier)
-        }
-        self._expressionViewModel = {
-            ExpressionViewModel(root: root, path: path.expressionValue, label: label, notifier: notifier)
-        }
-        self._enumeratedViewModel = {
-            EnumeratedViewModel(root: root, path: path.enumeratedValue, label: label, notifier: notifier)
-        }
-        self._lineViewModel = {
-            LineViewModel(root: root, path: path.lineValue, label: label, notifier: notifier)
-        }
-        super.init(root: root, path: path, defaultValue: .bool(false), notifier: notifier)
-    }
-
-    init(valueRef: Ref<LineAttribute>, errorsRef: ConstRef<[String]>, label: String, delayEdits: Bool) {
-        self._boolViewModel = {
-            BoolViewModel(valueRef: valueRef.boolValue, errorsRef: errorsRef, label: label)
-        }
-        self._integerViewModel = {
-            IntegerViewModel(valueRef: valueRef.integerValue, errorsRef: errorsRef, label: label, delayEdits: delayEdits)
-        }
-        self._floatViewModel = {
-            FloatViewModel(valueRef: valueRef.floatValue, errorsRef: errorsRef, label: label, delayEdits: delayEdits)
-        }
-        self._expressionViewModel = {
-            ExpressionViewModel(valueRef: valueRef.expressionValue, errorsRef: errorsRef, label: label, delayEdits: delayEdits)
-        }
-        self._enumeratedViewModel = {
-            EnumeratedViewModel(valueRef: valueRef.enumeratedValue, errorsRef: errorsRef, label: label)
-        }
-        self._lineViewModel = {
-            LineViewModel(valueRef: valueRef.lineValue, errorsRef: errorsRef, label: label, delayEdits: delayEdits)
-        }
-        super.init(valueRef: valueRef, errorsRef: errorsRef)
-    }
-
-}
-
+/// The view model associated with `LineAttributeView`.
+/// 
+/// This view model is reponsible for managing the state of a
+/// `LineAttributeView`. This view model also provides functionality that the
+/// `LineAttributeView` requires to function. The `LineAttributeView` displays
+/// a `LineAttribute`. Since a `LineAttribute` is an enum, and thus can be
+/// one of many different cases, this view model provides access to view models
+/// for each separate case.
 public final class LineAttributeViewModel: ObservableObject, Identifiable, GlobalChangeNotifier {
 
+    /// A reference to the line attribute that is associated with this view
+    /// model.
     private let ref: LineAttributeValue
 
+    /// The `BoolViewModel` when the attribute associated with this view model
+    /// is a bool property.
+    /// 
+    /// - Warning: If the attribute associated with this view model is not a
+    /// bool then utilising this property will cause a runtime error.
     lazy var boolViewModel: BoolViewModel = {
         ref.boolViewModel
     }()
 
+    /// The `IntegerViewModel` when the attribute associated with this view
+    /// model is an integer property.
+    /// 
+    /// - Warning: If the attribute associated with this view model is not an
+    /// integer then utilising this property will cause a runtime error.
     lazy var integerViewModel: IntegerViewModel = {
         ref.integerViewModel
     }()
 
+    /// The `FloatViewModel` when the attribute associated with this view
+    /// model is a float property.
+    /// 
+    /// - Warning: If the attribute associated with this view model is not a
+    /// float then utilising this property will cause a runtime error.
     lazy var floatViewModel: FloatViewModel = {
         ref.floatViewModel
     }()
 
+    /// The `ExpressionViewModel` when the attribute associated with this view
+    /// model is an expression property.
+    /// 
+    /// - Warning: If the attribute associated with this view model is not an
+    /// expression then utilising this property will cause a runtime error.
     lazy var expressionViewModel: ExpressionViewModel = {
         ref.expressionViewModel
     }()
 
+    /// The `EnumeratedViewModel` when the attribute associated with this view
+    /// model is an enumerated property.
+    /// 
+    /// - Warning: If the attribute associated with this view model is not an
+    /// enumerated property then utilising this property will cause a runtime
+    /// error.
     lazy var enumeratedViewModel: EnumeratedViewModel = {
         ref.enumeratedViewModel
     }()
 
+    /// The `LineViewModel` when the attribute associated with this view
+    /// model is a line property.
+    /// 
+    /// - Warning: If the attribute associated with this view model is not a
+    /// line then utilising this property will cause a runtime error.
     lazy var lineViewModel: LineViewModel = {
         ref.lineViewModel
     }()
 
+    /// The `LineAttribute` associated with this view model.
+    /// 
+    /// - Attention: Changing this value triggers an `objectWillChange`
+    /// notification to be sent. Depending on the type of the attribute,
+    /// an `objectWillChange` notification will also be triggered in the
+    /// sub-view-model.
     var lineAttribute: LineAttribute {
         get {
             ref.value
@@ -188,14 +163,68 @@ public final class LineAttributeViewModel: ObservableObject, Identifiable, Globa
         }
     }
 
-    public init<Root: Modifiable>(root: Ref<Root>, path: Attributes.Path<Root, LineAttribute>, label: String, notifier: GlobalChangeNotifier? = nil) {
+    /// Create a new `LineAttributeViewModel`.
+    /// 
+    /// This initialiser create a new `LineAttributeViewModel` utilising a key
+    /// path from a `Modifiable` object that contains the line attribute that
+    /// this view model is associated with.
+    /// 
+    /// - Parameter root: A reference to the base `Modifiable` object that
+    /// contains the line attribute that this view model is associated with.
+    /// 
+    /// - Parameter path: An `Attributes.Path` that points to the line attribute
+    /// from the base `Modifiable` object.
+    /// 
+    /// - Parameter label: The label to use when presenting the line attribute.
+    /// 
+    /// - Parameter notifier: A `GlobalChangeNotifier` that will be used to
+    /// notify any listeners when a trigger is fired.
+    public init<Root: Modifiable>(
+        root: Ref<Root>,
+        path: Attributes.Path<Root, LineAttribute>,
+        label: String,
+        notifier: GlobalChangeNotifier? = nil
+    ) {
         self.ref = LineAttributeValue(root: root, path: path, label: label, notifier: notifier)
     }
 
-    public init(valueRef: Ref<LineAttribute>, errorsRef: ConstRef<[String]>, label: String, delayEdits: Bool = false) {
-        self.ref = LineAttributeValue(valueRef: valueRef, errorsRef: errorsRef, label: label, delayEdits: delayEdits)
+    /// Create a new `LineAttributeViewModel`.
+    /// 
+    /// This initialiser create a new `LineAttributeViewModel` utilising a
+    /// reference to the line attribute directly. It is useful to call this
+    /// initialiser when utilising line attributes that do not exist within a
+    /// `Modifiable` object.
+    /// 
+    /// - Parameter valueRef: A reference to the line attribute that this
+    /// view model is associated with.
+    /// 
+    /// - Parameter errorsRef: A const-reference to the errors that are
+    /// associated with the line attribute.
+    /// 
+    /// - Parameter label: The label to use when presenting the line attribute.
+    /// 
+    /// - Parameter delayEdits: Delays edit notifications for those attributes
+    /// where it is applicable to do so (for example, delaying edits for a
+    /// line properties so that a notification is not sent for every
+    /// character change).
+    public init(
+        valueRef: Ref<LineAttribute>,
+        errorsRef: ConstRef<[String]>,
+        label: String,
+        delayEdits: Bool = false
+    ) {
+        self.ref = LineAttributeValue(
+            valueRef: valueRef,
+            errorsRef: errorsRef,
+            label: label,
+            delayEdits: delayEdits
+        )
     }
 
+    /// Manually trigger an `objectWillChange` notification.
+    /// 
+    /// This function recursively triggers an `objectWillChange` notification
+    /// to any child view models.
     public func send() {
         objectWillChange.send()
         if ref.isValid {
@@ -220,10 +249,20 @@ public final class LineAttributeViewModel: ObservableObject, Identifiable, Globa
 
 extension LineAttributeViewModel: Hashable {
 
-    public static func ==(lhs: LineAttributeViewModel, rhs: LineAttributeViewModel) -> Bool {
+    /// Is `lhs` equal to `rhs`.
+    /// 
+    /// - Parameter lhs: The `LineAttributeViewModel` on the left-hand side
+    /// of the equals sign.
+    /// 
+    /// - Parameter rhs: The `LineAttributeViewModel` on the right-hand side
+    /// of the equals sign.
+    public static func == (lhs: LineAttributeViewModel, rhs: LineAttributeViewModel) -> Bool {
         lhs.lineAttribute == rhs.lineAttribute
     }
 
+    /// Compute the hash of `self`.
+    /// 
+    /// - Parameter hasher: The `Hasher` to use to compute the hash.
     public func hash(into hasher: inout Hasher) {
         hasher.combine(lineAttribute)
     }
